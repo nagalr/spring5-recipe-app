410baef89e47720685b27a36c0fc691da92b26a8
Ronnen <nagalr@gmail.com>
2021-04-07 17:47:11 +0300

    Convert RecipeToRecipeCommandTest to a Unit test class (was Integration test brought-in the context)

--- src/test/java/guru/springframework/converters/RecipeToRecipeCommandTest.java
+++ src/test/java/guru/springframework/converters/RecipeToRecipeCommandTest.java
@@ -2,19 +2,12 @@ package guru.springframework.converters;
 
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.domain.*;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
-@SpringBootTest
-@RunWith(SpringRunner.class)
-@Slf4j
+
+
 public class RecipeToRecipeCommandTest {
 
     public static final Long RECIPE_ID = 1L;
@@ -33,7 +26,6 @@ public class RecipeToRecipeCommandTest {
     public static final Long NOTES_ID = 9L;
     RecipeToRecipeCommand converter;
 
-    private static final Logger logger = LoggerFactory.getLogger(RecipeToRecipeCommandTest.class);
 
     @Before
     public void setUp() throws Exception {
@@ -41,7 +33,6 @@ public class RecipeToRecipeCommandTest {
                 new CategoryToCategoryCommand(),
                 new IngredientToIngredientCommand(new UnitOfMeasureToUnitOfMeasureCommand()),
                 new NotesToNotesCommand());
-        logger.error("****************** Inside the setUp() method");
     }
 
     @Test
