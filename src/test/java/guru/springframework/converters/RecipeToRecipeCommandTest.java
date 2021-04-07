dfdcbdab3609f7d258ade94c2364f98a60d64bd6
Ronnen <nagalr@gmail.com>
2021-04-07 18:30:24 +0300

    Update the RecipeToRecipeCommandTest internal fields to 'private'

--- src/test/java/guru/springframework/converters/RecipeToRecipeCommandTest.java
+++ src/test/java/guru/springframework/converters/RecipeToRecipeCommandTest.java
@@ -10,20 +10,20 @@ import static org.junit.Assert.*;
 
 public class RecipeToRecipeCommandTest {
 
-    public static final Long RECIPE_ID = 1L;
-    public static final Integer COOK_TIME = Integer.valueOf("5");
-    public static final Integer PREP_TIME = Integer.valueOf("7");
-    public static final String DESCRIPTION = "My Recipe";
-    public static final String DIRECTIONS = "Directions";
-    public static final Difficulty DIFFICULTY = Difficulty.EASY;
-    public static final Integer SERVINGS = Integer.valueOf("3");
-    public static final String SOURCE = "Source";
-    public static final String URL = "Some URL";
-    public static final Long CAT_ID_1 = 1L;
-    public static final Long CAT_ID2 = 2L;
-    public static final Long INGRED_ID_1 = 3L;
-    public static final Long INGRED_ID_2 = 4L;
-    public static final Long NOTES_ID = 9L;
+    private static final Long RECIPE_ID = 1L;
+    private static final Integer COOK_TIME = Integer.valueOf("5");
+    private static final Integer PREP_TIME = Integer.valueOf("7");
+    private static final String DESCRIPTION = "My Recipe";
+    private static final String DIRECTIONS = "Directions";
+    private static final Difficulty DIFFICULTY = Difficulty.EASY;
+    private static final Integer SERVINGS = Integer.valueOf("3");
+    private static final String SOURCE = "Source";
+    private static final String URL = "Some URL";
+    private static final Long CAT_ID_1 = 1L;
+    private static final Long CAT_ID2 = 2L;
+    private static final Long INGRED_ID_1 = 3L;
+    private static final Long INGRED_ID_2 = 4L;
+    private static final Long NOTES_ID = 9L;
     RecipeToRecipeCommand converter;
 
 
